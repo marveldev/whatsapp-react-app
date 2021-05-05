04a951236f4e88c5486b092658d8a90bdb73feba
Marvel Dev <marvellousalika48@gmail.com>
2021-05-05 21:12:26 +0100

    create and design the contact list page

--- src/components/chatPage/ContactListPage.jsx
+++ src/components/chatPage/ContactListPage.jsx
@@ -1,7 +1,26 @@
+import contactList from './contactList'
+import './chatPage.scss'
+
 const ContactListPage = () => {
   return (
-    <div>
-
+    <div className="contact-list-page">
+      <div className="contact-list">
+        {contactList.map(({ id, name, profilePhoto }) => (
+          <div key={id} className="contact" role="button" tabIndex="0">
+            <div className="photo-container">
+              <img src={profilePhoto} className="contact-photo" alt="contact" />
+            </div>
+            <div className="info">
+              <p>{name}</p>
+              <div>
+                <span>Hello</span>
+                <span className="notification">2</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="message-icon"><i className="material-icons">&#xe0b7;</i></button>
     </div>
   )
 }
