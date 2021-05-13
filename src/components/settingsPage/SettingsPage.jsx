22c4c00415b721e0f25413e756ba4150c5aacc8e
Marvel Dev <marvellousalika48@gmail.com>
2021-05-13 13:16:32 +0100

    create and design settingsPage

--- src/components/settingsPage/SettingsPage.jsx
+++ src/components/settingsPage/SettingsPage.jsx
@@ -1,54 +1,58 @@
+import { useHistory } from 'react-router'
 import { CONSTANTS } from '../../common/constants'
+import './settingsPage.scss'
 
 const SettingsPage = () => {
+  const { goBack } = useHistory()
+
   return (
     <div className="settings-page">
       <div className="header">
-        <button className="material-icons">&#xe5c4;</button>
+        <button onClick={goBack} className="material-icons">&#xe5c4;</button>
         <p>Settings</p>
       </div>
       <div className="content">
-        <div>
+        <div className="user-profile">
           <div className="photo-container">
             <img src={CONSTANTS.PHOTOURL}
               className="photo" alt="profile" />
           </div>
-          <div>
-            <span>Jack Williams</span>
-            <p>Hey!, I am Jack.</p>
+          <div className="info">
+            <p>Jack Williams</p>
+            <span>Hey!, I am Jack.</span>
           </div>
         </div>
         <div>
-          <button>
-            <span className="fa fa-key"></span>
+          <button className="settings-option-button">
+            <i className="fa fa-key"></i>
             <div>
               <p>Account</p>
               <span>Privacy,security, change number</span>
             </div>
           </button>
-          <button>
-            <span className="material-icons">&#xe0b7;</span>
+          <button className="settings-option-button">
+            <i className="material-icons">&#xe0b7;</i>
             <div>
               <p>Chats</p>
               <span>Theme,wallpaper,chat history</span>
             </div>
           </button>
-          <button>
-            <span className="fa fa-bell"></span>
+          <button className="settings-option-button">
+            <i className="fa fa-bell"></i>
             <div>
               <p>Notification</p>
               <span>Message,group & call tones</span>
             </div>
           </button>
-          <button>
-            <span className="material-icons">&#xe1af;</span>
+          <button className="settings-option-button">
+            <i className="material-icons">&#xe1af;</i>
             <div>
               <p>Storage and data</p>
               <span>Network usuage, auto-download</span>
             </div>
           </button>
-          <button>
-            <span className="material-icons">&#xe8fd;</span>
+          <button className="settings-option-button">
+            <i className="material-icons">&#xe8fd;</i>
             <div>
               <p>Help</p>
               <span>FAQ, contact us, privacy policy</span>
