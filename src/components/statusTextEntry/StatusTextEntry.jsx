7d16dd507b1cdf195d1a4eca7ce6e079c93fb0e9
marveldev <marvellousalika48@gmail.com>
2021-05-18 17:15:39 +0100

    add function to generate random background color

--- src/components/statusTextEntry/StatusTextEntry.jsx
+++ src/components/statusTextEntry/StatusTextEntry.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { useHistory } from 'react-router'
-import { Smileys } from '../../common/components/Smileys'
+import { Smileys } from '../../common'
 import './statusTextEntry.scss'
 
 const StatusTextEntry = () => {
   const [smileyModalIsOpen, setSmileyModalIsOpen] = useState(false)
   const [statusInputValue, setStatusInputValue] = useState('')
   const [cursorPosition, setCursorPosition] = useState()
-  const randomColor = Math.floor(Math.random()*16777215).toString(16)
-  const [backgroundColor, setBackgroundColor] = useState("#")
+  const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+  const [backgroundColor, setBackgroundColor] = useState(randomColor)
+  const [fontFamily, setFontFamily] = useState()
   const { goBack } = useHistory()
 
   const toggleSmileyModalDisplay = event => {
@@ -17,8 +18,24 @@ const StatusTextEntry = () => {
   }
 
   const changeBackgroundColor = () => {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16)
-    setBackgroundColor("#" + randomColor)
+    let randomColor = '#'
+    for (var i = 0; i < 6; i++) {
+      randomColor += Math.floor(Math.random() * 10);
+    }
+
+    setBackgroundColor(randomColor)
+  }
+
+  const changeFontfamily = () => {
+    const fontFamilyList = [ 
+      "Zapf-Chancery", "Verdana", "Helvetica", "Roboto", "sans-serif",
+      "Times New Roman", "Courier", "Arial Narrow", "Candara",
+      "Calibri", "Garamond", "cursive", "Monaco",
+      "Brush Script MT", "Copperplate", "Courier New"
+    ]
+    const index = Math.floor(Math.random()*fontFamilyList.length)
+    console.log(fontFamilyList[index])
+    setFontFamily(fontFamilyList[index])
   }
 
   return (
@@ -28,21 +45,19 @@ const StatusTextEntry = () => {
           value={statusInputValue}
           onChange={event => setStatusInputValue(event.target.value)}
           placeholder="Type a status"
+          style={{fontFamily}}
           autoFocus
         >
         </textarea>
       </div>
       <div className="status-input-options">
-        <button
-          onClick={(event) => toggleSmileyModalDisplay(event)}
-        >
+        <button onClick={(event) => toggleSmileyModalDisplay(event)}>
           <i className="material-icons">&#xe7f2;</i>
         </button>
-        <button className="material-icons">&#xe165;</button>
-        <button
-          onClick={changeBackgroundColor}
-          className="material-icons"
-        >
+        <button onClick={changeFontfamily} className="material-icons">
+          &#xe165;
+        </button>
+        <button onClick={changeBackgroundColor} className="material-icons">
           &#xe40a;
         </button>
       </div>
