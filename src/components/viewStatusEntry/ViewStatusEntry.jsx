937603da04e00ed8a27e2117ef19e028738689d8
marveldev <marvellousalika48@gmail.com>
2021-05-20 08:14:16 +0100

    add function to display next status
    - add function to display previous status

--- src/components/viewStatusEntry/ViewStatusEntry.jsx
+++ src/components/viewStatusEntry/ViewStatusEntry.jsx
@@ -1,14 +1,59 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 import { constants } from '../../common'
+import { statusActions } from '../statusPage/slice'
 import './viewStatusEntry.scss'
 
 const ViewStatusEntry = () => {
+  const { statusData, currentStatusIndex } = useSelector(state => state.status)
   const { goBack } = useHistory()
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    const progressBars = document.querySelectorAll('.progress-bar')
+    console.log(progressBars);
+    // let interval
+    // for (let index = 0; index < bars.length; index++) {
+    //   const bar = bars[index]
+    //   let width = 1
+    //   setInterval(() => {
+    //     if (width >= 100) {
+
+    //     } else {
+    //       width++
+    //       bar.style.width = width + '%'
+    //     }
+    //   }, 30)
+    // }
+  }, [])
+
+  const displayNextStatus = () => {
+    if (currentStatusIndex === statusData.length - 1) {
+      dispatch(statusActions.setCurrentStatusIndex(0))
+    } else {
+      dispatch(statusActions.setCurrentStatusIndex(currentStatusIndex + 1))
+    }
+  }
+
+  const displayPreviousStatus = () => {
+    if (currentStatusIndex === 0) {
+      dispatch(statusActions.setCurrentStatusIndex(statusData.length - 1))
+    } else {
+      dispatch(statusActions.setCurrentStatusIndex(currentStatusIndex - 1))
+    }
+  }
 
   return (
     <div className="view-status-entry">
       <div className="header">
-        <div className="progress-bar"><div className="bar"></div></div>
+        <div>
+          {statusData.map((item, index) => (
+            <div key={index} className="progress-bar">
+              <div className="bar"></div>
+            </div>
+          ))}
+        </div>
         <div onClick={goBack} className="user-profile">
           <button className="material-icons">&#xe5c4;</button>
           <div className="photo-container">
@@ -22,10 +67,10 @@ const ViewStatusEntry = () => {
       </div>
       <div className="content">
         <div className="status-entry-container">
-          <img src={constants.PHOTOURL} alt="status" />
+          {/* <img src={statusData[currentStatusIndex]?.photoSource} alt="status" /> */}
         </div>
-        <button className="previous-button">previous</button>
-        <button className="next-button">next</button>
+        <button onClick={displayPreviousStatus} className="previous-button">previous</button>
+        <button onClick={displayNextStatus} className="next-button">next</button>
       </div>
     </div>
   )
