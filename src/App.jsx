b3dba0525d779d5790d941fadf895a033d06918c
marveldev <marvellousalika48@gmail.com>
2021-06-03 16:47:25 +0100

    add state for setting device

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { BrowserRouter, Switch } from "react-router-dom"
+import { BrowserRouter, Switch, useHistory } from "react-router-dom"
 import MobileRoutes from './components/mobile/route'
 import DesktopRoutes from './components/desktop/route'
 import { getChats, getWallpaper } from './components/mobile/chatPage/slice'
 import { getProfile } from './components/mobile/profilePage/slice'
 import { getStatus } from './components/mobile/statusPage/slice'
 import './index.scss'
+import { useState } from 'react'
 
 const App = () => {
   const { theme } = useSelector(state => state.displaySettings)
   const currentTheme = theme?.toLowerCase()
+  const [device, setDevice] = useState()
   const dispatch = useDispatch()
+  const history = useHistory()
 
   useEffect(() => {
+    if (window.innerWidth >= 768) {
+      setDevice('desktop')
+    } else {
+      setDevice('mobile')
+    }
+
     dispatch(getProfile())
     dispatch(getChats())
     dispatch(getWallpaper())
     dispatch(getStatus())
-  }, [dispatch])
+  }, [dispatch, history])
 
   return (
     <BrowserRouter>
       <div className={`app-layer ${currentTheme}`}>
-        <Switch>
-          {/* <MobileRoutes /> */}
-          <DesktopRoutes />
-        </Switch>
+        {device === 'mobile' && (
+          <Switch>
+            <MobileRoutes />
+          </Switch>
+        )}
+        {device === 'desktop' && (
+          <Switch>
+            <DesktopRoutes />
+          </Switch>
+        )}
       </div>
     </BrowserRouter>
   )
