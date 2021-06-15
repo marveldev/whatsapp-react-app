45cedc9896abc48b2a7a9fa45c10076d474740ef
marveldev <marvellousalika48@gmail.com>
2021-06-15 12:57:02 +0100

    add functionality to delete status entry after 24 hours

--- src/components/desktop/statusPage/StatusPage.jsx
+++ src/components/desktop/statusPage/StatusPage.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import StatusPane from './StatusPane'
 import StatusGallery from './StatusGallery'
+import database from '../../../database'
 import './statusPage.scss'
 
 const StatusPage = () => {
+  const { statusData } = useSelector(state => state.status)
+
+  useEffect(() => {
+    statusData.map(async ({storedTime, id}) => {
+      const timeDifference = new Date().getTime() - storedTime
+      const statusDuration = Math.floor(timeDifference/1000/60/60)
+      if (statusDuration >= '24') {
+        await database.status.delete(id)
+      }
+    })
+  }, [statusData])
+
   return (
     <div className="desktop-status-page">
       <div>
