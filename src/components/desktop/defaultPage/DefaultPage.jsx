caafb639ed517e09b0a762410d7a273a896d724f
marveldev <marvellousalika48@gmail.com>
2021-06-17 14:00:41 +0100

    add dark theme design for default pane and default page component

--- src/components/desktop/defaultPage/DefaultPage.jsx
+++ src/components/desktop/defaultPage/DefaultPage.jsx
@@ -1,11 +1,16 @@
-import { defaultLightPagePhoto } from '../../../common'
+import { useSelector } from 'react-redux'
+import { defaultLightPagePhoto, defaultDarkPagePhoto } from '../../../common'
 import './defaultPage.scss'
 
 const DefaultPage = () => {
+  const { theme } = useSelector(state => state.displaySettings)
+
   return (
     <div className="default-page">
       <div>
-        <img src={defaultLightPagePhoto} alt="phone-in-hand" />
+        <img src={theme === 'Dark' ? defaultDarkPagePhoto : defaultLightPagePhoto}
+          alt="phone-in-hand"
+        />
         <p>Keep your phone connected</p>
         <span>WhatsApp connects to your phone to sync messages.
           To reduce data usuage, connect your phone to Wi-Fi.
